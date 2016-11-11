aaf396438bb49b12ac1ec2e36fbf34b429d7bec8
ssingh19 <sumeet.aussie@gmail.com>
2016-11-10 18:39:19 -0800

    Speeding up metric comps

--- call_find_metric_PVTOL.m
+++ call_find_metric_PVTOL.m
@@ -125,7 +125,7 @@ B_perp = [eye(4);
 
 % d_bar = sqrt(double(1/w_lower))/lambda; %normalized
 
-ctrl_N = 12;
+ctrl_N = 20;
 p_range = linspace(-p_lim, p_lim, ctrl_N);
 vy_range = linspace(-vy_lim, vy_lim, ctrl_N);
 vz_range = linspace(-vz_lim, vz_lim, ctrl_N);
@@ -166,7 +166,7 @@ for i = 1:length(p_range)
             for l = 1:length(pd_range)
                 x = [0;0;p_range(i);vy_range(j);vz_range(k);pd_range(l)];
                 
-                W = W_mat(x);
+                W = symmetric(W_mat(x));
                 M = W\eye(n);
                 Theta = chol(M);
                 Theta_Bw = Theta*Bw(x);
@@ -176,9 +176,9 @@ for i = 1:length(p_range)
                 
                 f = f_mat(x);
                 df = df_mat(x);
-                F = -dW_p_mat(x)*f(1) - dW_vy_mat(x)*f(2)-...
-                     dW_vz_mat(x)*f(3) - dW_pd_mat(x)*f(4) + ...
-                     df*W + W*df' + 2*lambda*W;
+                F = -symmetric(dW_p_mat(x))*f(1) - symmetric(dW_vy_mat(x))*f(2)-...
+                               dW_vz_mat(x)*f(3) - dW_pd_mat(x)*f(4) + ...
+                               df*W + W*df' + 2*lambda*W;
                 
                 delta_u_den = eig((inv(L))'*(B*B')*inv(L));
                 delta_u(i,j,k,l) = 0.5*max(eig((inv(L))'*F*inv(L)))/...
